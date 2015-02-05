a5e87353467189e2b9886830739613b38dcb9723
ATofighi <alirtofighim@gmail.com>
2015-02-06 00:26:20 +0330

    Add Ctrl+F11 to Maximize

--- editor/plugins/maximize.js
+++ editor/plugins/maximize.js
@@ -1,6 +1,6 @@
 ATEditor.plugins['miximize'] = function()
 {
-	ATEditor.addbutton('maximize', 'Maximize', {
+	ATEditor.addbutton('maximize', 'Maximize | Ctrl + F11', {
 		extraclass: 'fa fa-arrows-alt',
 		trigger: true,
 		active: function()
@@ -16,5 +16,11 @@ ATEditor.plugins['miximize'] = function()
 		isactive: function() {
 			return ATEditor.$editor.hasClass('ate_maximize');
 		}
+	});
+
+	ATEditor.addKey('ctrl+f11', function()
+	{
+		ATEditor.runHook('events.run.maximize');
+		return false;
 	});
 };
